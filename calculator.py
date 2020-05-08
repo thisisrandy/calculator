e22c6468efb9c73fcc4f99d5ca50681e1d35bd27
Randy West <randywest55@gmail.com>
2020-05-08 17:49:51 -0400

    Refactor out early parser

--- calculator.py
+++ calculator.py
@@ -1,62 +1,11 @@
 # %%
 from enum import Enum, auto
 import re
-from orderedset import OrderedSet
 from terminals import Terminals
 from nonterminals import NonTerminals
 from lexer import lex
 from production import Production
-from defaultgrammar import DEFAULT_GRAMMAR
-
-# %%
-class EarleyParser:
-    """
-    implementation of
-    https://en.wikipedia.org/wiki/Earley_parser#Pseudocode
-    """
-
-    def __init__(self, tokens, grammar=GRAMMAR):
-        """
-        grammar is a tuple of Productions, where the first production is
-        assumed to be the top level
-        """
-        self.tokens = tokens
-        if len(grammar) == 0:
-            raise Exception("grammar must contain at least one production")
-        self.grammar = grammar
-
-    def parse(self):
-        tokens = self.tokens
-        top_level = self.grammar[0]
-        S = self.S = [OrderedSet() for _ in range(len(tokens) + 1)]
-        S[0].add(top_level)
-
-        for k in range(len(tokens) + 1):
-            while S[k].hasNext():
-                state = S[k].next()
-                if not state.isComplete():
-                    if not state.isNextTerminal():
-                        self._predictor(state, k)
-                    else:
-                        self._scanner(state, k)
-                else:
-                    self._completer(state, k)
-
-        return top_level.asComplete() in S[len(tokens)]
-
-    def _predictor(self, state, k):
-        for rule in filter(lambda r: r.nameMatches(state.next()), self.grammar):
-            self.S[k].add(rule.copy(k))
-
-    def _scanner(self, state, k):
-        if state.nextMatches(self.tokens[k]):
-            self.S[k + 1].add(state.advance())
-
-    def _completer(self, state, k):
-        S = self.S
-        for production in filter(lambda p: p.nextMatches(state.name), S[state.origin]):
-            S[k].add(production.advance())
-
+from earlyparser import EarleyParser
 
 # %%
 tokens = list(lex("1+2*3/4"))
