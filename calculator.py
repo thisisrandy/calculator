1762145050161faeb350560df163f922e959149e
Randy West <randywest55@gmail.com>
2020-05-08 17:34:29 -0400

    Refactor out lexer

--- calculator.py
+++ calculator.py
@@ -4,48 +4,7 @@ import re
 from orderedset import OrderedSet
 from terminals import Terminals
 from nonterminals import NonTerminals
-
-# %%
-class Token:
-    def __init__(self, type, value=None):
-        self.type = type
-        self.value = value
-
-    def __eq__(self, other):
-        """equality by type, not value"""
-        return self.type == other
-
-    def __repr__(self):
-        return f"{self.type} {self.value}"
-
-
-def lex(expression):
-    accum = ""
-    for char in expression:
-        if re.match("\d", char):
-            accum += char
-        else:
-            if len(accum) > 0:
-                yield Token(Terminals.LITERAL, accum)
-                accum = ""
-            if re.match("\s", char):
-                continue
-            try:
-                type = {
-                    "+": Terminals.PLUS,
-                    "-": Terminals.MINUS,
-                    "*": Terminals.MULT,
-                    "/": Terminals.DIV,
-                    "(": Terminals.RPAREN,
-                    ")": Terminals.LPAREN,
-                }[char]
-                yield Token(type, char)
-            except:
-                raise Exception(f"Invalid character {char} in expression {expression}")
-    if len(accum) > 0:
-        yield Token(Terminals.LITERAL, accum)
-    yield Token(Terminals.EOF)
-
+from lexer import lex
 
 # %%
 class Production:
