25fdb0dc046af55128e3d478d37048d0ef2b40ce
Randy West <randywest55@gmail.com>
2020-05-09 18:41:56 -0400

    Refactor lexer tests out to separate file

--- earlyparser_test.py
+++ earlyparser_test.py
@@ -1,21 +1,9 @@
-# tests of lex/parse correctness
+# tests of parse correctness
 
 from lexer import lex
 from earlyparser import EarleyParser
 from parseerror import ParseError
 import pytest
-
-
-def test_lex():
-    with pytest.raises(ParseError):
-        list(lex("1+2f"))
-    with pytest.raises(ParseError):
-        list(lex("hello!"))
-    with pytest.raises(ValueError):
-        list(lex("1.1.1"))
-    with pytest.raises(ValueError):
-        list(lex("1e-1.2"))
-    assert len(list(lex("(1+2.4)*1e-2/(8-9)"))) == 14
 
 
 def test_parse():
