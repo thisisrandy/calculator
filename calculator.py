3a397aba3e9c5d47a94265ddd25f56641b59c0df
Randy West <randywest55@gmail.com>
2020-05-09 17:41:42 -0400

    Convert calculator to repl

--- calculator.py
+++ calculator.py
@@ -1,15 +1,27 @@
-# %%
-from enum import Enum, auto
-import re
-from terminals import Terminals
-from nonterminals import NonTerminals
-from lexer import lex
-from production import Production
-from earlyparser import EarleyParser
+if __name__ == "__main__":
+    from enum import Enum, auto
+    import re
+    from terminals import Terminals
+    from nonterminals import NonTerminals
+    from lexer import lex
+    from production import Production
+    from earlyparser import EarleyParser
+    from parseerror import ParseError
+    import signal
+    import sys
 
-# %%
-tokens = list(lex("1+-2*(3/4-1.0)+13e-2"))
-parser = EarleyParser(tokens)
-res = parser.parse()
-if res:
-    print(res.evaluate())
+    signal.signal(signal.SIGINT, lambda sig, frame: sys.exit(0))
+
+    while True:
+        try:
+            user_input = input("Enter an expression: ")
+            tokens = list(lex(user_input))
+            parser = EarleyParser(tokens)
+            res = parser.parse()
+            if not res:
+                raise ParseError(f"Unable to parse {user_input}")
+            print(f"Result: {res.evaluate()}")
+        except EOFError:
+            sys.exit(0)
+        except Exception as e:
+            print(e)
