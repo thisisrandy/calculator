7b3008b5f073d61073566c3c8817adef5f26bf9a
Randy West <randywest55@gmail.com>
2020-05-09 16:29:00 -0400

    Remove unused AST class

--- calculator.py
+++ calculator.py
@@ -13,29 +13,3 @@ parser = EarleyParser(tokens)
 res = parser.parse()
 if res:
     print(res.evaluate())
-
-# %%
-class AST:
-    class Node:
-        def __init__(self, symbol, parent=None, child_left=None, child_right=None):
-            self.symbol = symbol
-            self.parent = parent
-            self.child_left = child_left
-            self.child_right = child_right
-
-    def __init__(self, expression):
-        """
-        see https://en.wikipedia.org/wiki/Earley_parser#Pseudocode
-
-        parsing:
-        - try to match current token to left-most unmatched node
-        - if we succeed, advance to next token
-        - otherwise
-            - if the rightmost node is not a terminal, expand it and try again
-            - if the rightmost node is a terminal, back up until an alternate
-            expansion is found, or fail
-
-        the stack:
-
-        """
-        self.root = AST.Node(NonTerminals.GOAL)
